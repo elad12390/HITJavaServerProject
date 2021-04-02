2086ca4b69069cb0fe24a60e01a1e9316e341e96
kim kim <kimlevy06@gmail.com>
2021-04-02 11:58:27 +0300

    started working on kmp היי לדי תיקנתי כמה דברים שכתבתי לא נכון אתמול באלגורית כי מיהרתי, בגדול לא סיימתי אבל לקחתי תוכנית דוגמה מהאינרנט לראות שעד כה לפחות זה פועל בסדר, אז קיצר רק רציתי לתקן את השטויות של אתמול, נמשיך מתי שיסתדר לנו לעבוד על זה ביחד.

--- src/main/java/com/hit/string/matching/KmpSearch.java
+++ src/main/java/com/hit/string/matching/KmpSearch.java
@@ -1,52 +1,63 @@
 package main.java.com.hit.string.matching;
 import main.java.com.hit.string.matching.abstracts.AbstractMatcher;
 
-public class KmpSearch extends AbstractMatcher{
-    private int [] lpsArray(String pat, int [] lps){
-        int patLen=pat.length();
-        int lenLongestPs=0;
-        int i=1;
-        lps[0]=0;
-        while(i < patLen) {
-            if(pat.charAt(i) == pat.charAt(patLen)){
-                patLen++;
-                lps[i]=patLen;
+public class KmpSearch extends AbstractMatcher {
+    private int[] lpsArray(String pat, int[] lps) {
+        int patLen = pat.length();
+        int lenLongestPs = 0;
+        int i = 1;
+        lps[0] = 0;
+        while (i < patLen) {
+            if(pat.charAt(i)==pat.charAt(lenLongestPs)) {
+                lenLongestPs++;
+                lps[i]=lenLongestPs;
                 i++;
             }
             else {
-                if(patLen!=0) {
-                  patLen=lps[patLen-1];
+                if (lenLongestPs != 0) {
+                    lenLongestPs = lps[lenLongestPs - 1];
+                } else {
+                    lps[i] = lenLongestPs;
+                    i++;
                 }
-                lps[i]=patLen;
-                i++;
             }
+
         }
         return lps;
     }
+
     @Override
     public Integer match(String searchWord, String input) {
         Integer edgeCases = super.match(searchWord, input);
         if (edgeCases != null) return edgeCases;
-        int S=searchWord.length();
-        int N=input.length();
-        int [] lps = new int[S];
-        lpsArray(searchWord,lps);
-        int j,i=0;
-        while(i< N){
+        int S = searchWord.length();
+        int N = input.length();
+        int[] lps = new int[S];
+        lpsArray(searchWord, lps);
+        int j = 0, i = 0;
+        while (i < N) {
             if (searchWord.charAt(j) == input.charAt(i)) {
                 j++;
                 i++;
             }
-            if (j==S){
-                return (i-j);
-            }
-            else if(j!=0){
-                j=lps[j-1];
+            if (j == S) {
+                j = lps[j - 1];
+            } else if (i < N && searchWord.charAt(j) != input.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i = i + 1;
+                }
             }
         }
-
-
-
         return -1;
     }
+
+
+    public static void main(String[] args) {
+        String txt="AAAAAABBABABSDJDCABSNSJ";
+        String pat="DJDCA";
+        new KmpSearch().match(pat,txt);
+    }
+
 }
