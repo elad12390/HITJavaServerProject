c829ccda696e2f727d7d45d52fd7360bacf68815
kim kim <kimlevy06@gmail.com>
2021-04-06 16:50:52 +0300

    move folders

---
+++ GameCalendar/src/main.java
@@ -0,0 +1,8 @@
+import main.java.com.hit.stringmatching.implementations.KnuthMorrisPrattAlgoMatcherImpl;
+
+public class main {
+    public static void main(String[] args) {
+        var matcher = new KnuthMorrisPrattAlgoMatcherImpl();
+        matcher.match("sap","fjhskdfjgsfsgfbsapfsdghfkjfgh");
+    }
+}
